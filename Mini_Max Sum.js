6e521ea709ff5957f86fcb0f09224a19cdba3a16
Abdilmajid Ibrahim <abdilmajidibrahim@gmail.com>
2019-08-20 17:15:21 -0400

    update MinMax

--- Mini_Max Sum.js
+++ Mini_Max Sum.js
@@ -10,6 +10,13 @@ function minMax(arr){
   console.log(min,max)
 }
 
+// Better solution: Assuming array with 5 elements
+function minMax2(arr){
+  let total = arr.reduce((a,b) => a+b, 0);
+  let max = total - Math.max(...arr)
+  let min = total - Math.min(...arr)
+  console.log(min,max)
+}
 
 
-minMax([1,3,5,7,9])
+minMax2([1,3,5,7,9])
