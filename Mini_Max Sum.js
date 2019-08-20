9a1ebcd3e8dab938af477c02361dc4a44fd0d0af
Abdilmajid Ibrahim <abdilmajidibrahim@gmail.com>
2019-08-20 13:38:29 -0400

    MinMaxSum

---
+++ Mini_Max Sum.js
@@ -0,0 +1,14 @@
+function minMax(arr){
+  let minSort = arr.sort((a,b) => a - b)
+  let maxSort = [...minSort].reverse()
+  let min = 0
+  let max = 0
+  for(let i=0;i<=3;i++){
+    min += minSort[i]
+    max += maxSort[i] 
+  }
+  console.log(min,max)
+}
+
+
+minMax([1,3,5,7,9])
