6aa1095f3fa8c1c6c1e2a68316de7f0ab7518c18
Abdilmajid Ibrahim <abdilmajidibrahim@gmail.com>
2019-08-26 18:01:03 -0400

    items in array fix

--- First & Last Position of item in Sorted Array.js
+++ First & Last Position of item in Sorted Array.js
@@ -1,17 +1,24 @@
 const arr = [5,5,7,7,8,8,8,8,8,10,11,12,13,40]
+const arr2 = [2,2]
 
 function ar(arr,target){
   let start = arr.indexOf(target)
-  let count = 0
+  // let count = 0
   let last = 0
   for(let i=start;i<=arr.length;i++){
-    if(arr[i] === target) count++
-    if(arr[i]>target) {
-      last = i-1
+    // if(arr[i] === target) count++
+    if(arr[i+1]!==target) {
+      last = i
+      break
+    }
+    if(start === -1){
+      last = -1
       break
     }
   }
-  return [start,last,count]
+  return [start,last]
+
 }
 
-console.log(ar(arr,8))
+console.log(ar(arr,2))
+
